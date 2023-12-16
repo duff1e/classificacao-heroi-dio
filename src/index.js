949a21d41cc31b10780d1425f072239b9d7eda40
Duffie <51765144+duff1e@users.noreply.github.com>
2023-12-16 18:58:32 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ while (rankDoHeroi != "Radiante") {
   XPRecebida = Math.floor(Math.random() * (max - min + 1)) + min;
   XPTotal = XPTotal + XPRecebida
 
-  let mensagemDeXP = "Você ganhou " + XPRecebida + " de XP, totalizando " + XPTotal + " de XP. "
+  let mensagemDeXP = "Você ganhou " + XPRecebida + " de XP, totalizando " + XPTotal + " de XP."
 
   if (XPTotal <= 1000) {
     rankDoHeroi = "Ferro"
